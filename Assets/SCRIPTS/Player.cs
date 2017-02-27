aa4bdf2aa63639761e14596ff8003892c6d770fc
Keryann Malacchina <keryannm@gmail.com>
2017-02-27 15:33:16 +0100

    Testing Touch / Mouse Inputs

---
+++ Assets/SCRIPTS/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+	public GameObject test;
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			Instantiate (test, mainCamera.ScreenToWorldPoint (Input.GetTouch (0).position) + mainCamera.transform.position, Quaternion.identity);
+		}
+
+		if(Input.GetMouseButtonDown (0))
+		{
+			Instantiate (test, mainCamera.ScreenToWorldPoint (Input.mousePosition) + mainCamera.transform.position, Quaternion.identity);
+		}
+	}
+}
