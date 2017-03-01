0bc75da8d25c62c7d434e36e85b9c391778c7832
Keryann Malacchina <keryannm@gmail.com>
2017-03-01 18:18:15 +0100

    Working On Enemies

---
+++ Assets/SCRIPTS/Enemies/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	private static float _jumpForce = 10;
+	private Rigidbody _playerRigidbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_playerRigidbody = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	protected virtual void OnCollisionEnter (Collision collision)
+	{
+		if(collision.gameObject.tag == "Player")
+			HitByPlayer ();
+
+		if (LayerMask.NameToLayer ("Rocket") == collision.gameObject.layer)
+			Death ();
+	}
+
+	protected virtual void HitByPlayer ()
+	{
+		_playerRigidbody.AddForce (Vector3.up * _jumpForce, ForceMode.Impulse);
+
+		Death ();
+	}
+
+	protected virtual void Death ()
+	{
+		Destroy (gameObject);
+	}
+}
