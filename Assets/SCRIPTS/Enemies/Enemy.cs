adae32d5e10f20de4eb0fb936d63a2264d5955a5
Unknown <l.bourdrel@laposte.net>
2017-03-03 14:43:01 +0100

    Enemy_Walk

--- Assets/SCRIPTS/Enemies/Enemy.cs
+++ Assets/SCRIPTS/Enemies/Enemy.cs
@@ -21,18 +21,18 @@ public class Enemy : MonoBehaviour
 
 	protected virtual void OnCollisionEnter (Collision collision)
 	{
-		if(collision.gameObject.tag == "Player")
-			HitByPlayer ();
-
+		
 		if (LayerMask.NameToLayer ("Rocket") == collision.gameObject.layer)
 			Death ();
-	}
+
+        if (collision.gameObject.tag == "Player")
+            HitByPlayer();
+
+    }
 
 	protected virtual void HitByPlayer ()
 	{
-		_playerRigidbody.AddForce (Vector3.up * _jumpForce, ForceMode.Impulse);
-
-		Death ();
+        GameManager.Instance.GameOver();
 	}
 
 	protected virtual void Death ()
