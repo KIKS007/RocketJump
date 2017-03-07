8b680305d17eea39c11f4428354988b57e88675a
ETU\s.houzali <daezr.choko@gmail.com>
2017-03-07 16:46:04 +0100

    LD FInished Sly

---
+++ Assets/SCRIPTS/Collectibles/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour 
+{
+	public int Value = 1;
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject.tag == "Player")
+			CollectiblesManager.Instance.CollectiblePickedUp (Value);
+
+		Destroy (gameObject);
+	}
+}
