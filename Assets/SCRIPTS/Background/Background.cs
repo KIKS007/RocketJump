19a0c3f023211078b3a01bf6d6e1356034d308c4
Keryann Malacchina <keryannm@gmail.com>
2017-03-07 21:37:58 +0100

    Background

---
+++ Assets/SCRIPTS/Background/Background.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Background : MonoBehaviour 
+{
+	public float duration;
+	public float finaleYPos;
+
+	void Start ()
+	{
+		transform.DOLocalMoveY (finaleYPos, duration);
+	}
+}
