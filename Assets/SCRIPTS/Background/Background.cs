ef2a12d238a98f4e0afe315445c042bf9f7f1c3c
Keryann Malacchina <keryannm@gmail.com>
2017-03-07 22:23:52 +0100

    Background Done

--- Assets/SCRIPTS/Background/Background.cs
+++ Assets/SCRIPTS/Background/Background.cs
@@ -5,11 +5,21 @@ using DG.Tweening;
 
 public class Background : MonoBehaviour 
 {
-	public float duration;
-	public float finaleYPos;
+	public float Speed;
+	private ParallaxCamera _parallaxCamera;
+
+	/*public float duration;
+	public float finaleYPos;*/
 
 	void Start ()
 	{
-		transform.DOLocalMoveY (finaleYPos, duration);
+		_parallaxCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<ParallaxCamera> ();
+
+		//transform.DOLocalMoveY (finaleYPos, duration);
+	}
+
+	void Update ()
+	{
+		transform.Translate (Vector3.forward * Speed * _parallaxCamera.delta * Time.deltaTime);
 	}
 }
